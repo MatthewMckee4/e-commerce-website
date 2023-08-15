53384d91e7b16caa54ea1075f5cb361a0b87ebed
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2023-08-15 17:29:09 +0100

    added user input

--- app/app.py
+++ app/app.py
@@ -1,45 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Name %r>" & self.id
 
 
 @app.route("/")
-def hello():
-    return "Hello, Flask World!"
+def index():
+    return render_template("index.html")
 
 
-@app.route("/create_table")
-def create_table():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT)"""
-    )
-    conn.commit()
-    conn.close()
-    return "Table created successfully"
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
-@app.route("/add_user/<username>")
-def add_user(username):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (username) VALUES (?)", (username,))
-    conn.commit()
-    conn.close()
-    return f"User {username} added successfully"
+@app.route("/forgot")
+def forgot_password():
+    return render_template("forgot_password.html")
 
 
-@app.route("/users")
-def list_users():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users")
-    users = cursor.fetchall()
-    conn.close()
-    return render_template("users.html", users=users)
+@app.route("/users", methods=["POST", "GET"])
+def users():
+    if request.method == "POST":
+        user_name = request.form["name"]
+        new_friend = User(name=user_name)
+
+        try:
+            db.session.add(new_friend)
+            db.session.commit()
+            return redirect("/users")
+        except:
+            return "There was an Error Adding the User"
+    else:
+        users = User.query.order_by(User.date_created)
+        return render_template("users.html", users=users)
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
--- app/user/user.py
+++ app/user/user.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
 from typing import List
-from order import Order
+from order.order import Order
 
 
 @dataclass
 class User:
+    id: int
     username: str
-    email: str
-    password_hash: str
-    shipping_address: str
-    order_history: List[Order] = None
+    # email: str
+    # password_hash: str
+    # shipping_address: str
+    # order_history: List[Order] = None
